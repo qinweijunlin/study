08a72a679e69076cd84e1992c1224e101685a53a
Ivan <644633699@qq.com>
2020-09-11 22:28:37 +0800

    发布

---
+++ alien_invasion/alien_invasion.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+
+# 屏蔽 Pygame 的欢迎信息
+import os
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'ivan'
+
+import pygame
+
+def run_game():
+    # 初始化游戏并创建一个屏幕对象
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption("Alien Invasion")
+
+    # 开始游戏的主循环
+    while True:
+
+        # 监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # 让最近绘制的屏幕可见
+        pygame.display.flip()
+
+run_game()
