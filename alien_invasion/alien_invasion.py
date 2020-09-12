bbd792bda3688b03da7212454d5371592dc86998
Ivan <644633699@qq.com>
2020-09-12 17:59:35 +0800

    发布

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -9,6 +9,7 @@ os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = 'ivan'
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 def run_game():
     # 初始化 Pygame, 设置和屏幕对象
@@ -16,6 +17,9 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
+
+    # 创建一艘飞船
+    ship = Ship(screen)
 
     # 开始游戏的主循环
     while True:
@@ -27,6 +31,7 @@ def run_game():
 
         # 每次循环时都重绘屏幕
         screen.fill(ai_settings.bg_color)
+        ship.blitme()
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
